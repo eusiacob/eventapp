21ea162c4878cc732ba259b1fa028b8fbdeead41
eusebiu <eus.iacob@gmail.com>
2026-09-12 17:13:50 +0300

    adaugare link-uri la notificari. - toate - necitite - marcheaza toate ca fiind citite

--- src/main/java/com/example/eventapp/controller/GlobalModelAttributes.java
+++ src/main/java/com/example/eventapp/controller/GlobalModelAttributes.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
+
 @ControllerAdvice
 public class GlobalModelAttributes {
 
