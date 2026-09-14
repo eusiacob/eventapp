e3ec0fde71292e4778f3a39b8d8757634e5ec607
eusebiu <eus.iacob@gmail.com>
2026-09-14 23:24:22 +0300

    added mre categories

--- src/main/java/com/example/eventapp/model/BusinessCategory.java
+++ src/main/java/com/example/eventapp/model/BusinessCategory.java
@@ -6,17 +6,20 @@ import lombok.Getter;
 public enum BusinessCategory {
 
     FOTOGRAF("Fotograf", "bi-camera-fill"),
+    VIDEOGRAF("Videograf", "bi-camera-reels-fill"),
     DJ("DJ", "bi-music-note-beamed"),
-    MUZICA_LIVE("Muzică", "bi-mic-fill"),
+    MUZICA_LIVE("Muzică live", "bi-mic-fill"),
     RESTAURANT("Restaurant", "bi-cup-hot-fill"),
     CANDY_BAR("Candy Bar", "bi-cake2-fill"),
-    FLORIST("Florist", "bi-flower1"),
+    CATERING("Catering", "bi-egg-fried"),
+    FLORIST("Aranjamente florale", "bi-flower1"),
     DECOR("Decor", "bi-stars"),
     DIVERTISMENT("Divertisment", "bi-stars"),
-    ORGANIZATOR("Organizator", "bi-calendar-heart"),
+    LOCATII("Locații pentru evenimente", "bi bi-building-fill"),
+    ORGANIZATOR("Organizator eveniment", "bi-calendar-heart"),
     TRANSPORT("Transport", "bi-car-front-fill"),
     ARTIFICII("Artificii", "bi-stars"),
-    ALTELE("Altele", "bi-grid");
+    ALTELE("Alte servicii", "bi-grid");
 
     private final String displayName;
     private final String icon;
